873f5b8dac4422d49c4025a6e7b1cd401926b58a
Joris van der Wel <joris@jorisvanderwel.com>
2014-10-25 20:35:43 +0200

    Improved coverage for WEAPON_SLOT

--- src/test/java/aphelion/shared/physics/WEAPON_SLOTTest.java
+++ src/test/java/aphelion/shared/physics/WEAPON_SLOTTest.java
@@ -1,84 +1,85 @@
 /*
  * Aphelion
- * Copyright (c) 2013  Joris van der Wel
- * 
+ * Copyright (c) 2014  Joris van der Wel
+ *
  * This file is part of Aphelion
- * 
+ *
  * Aphelion is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, version 3 of the License.
- * 
+ *
  * Aphelion is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with Aphelion.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * In addition, the following supplemental terms apply, based on section 7 of
  * the GNU Affero General Public License (version 3):
  * a) Preservation of all legal notices and author attributions
  * b) Prohibition of misrepresentation of the origin of this material, and
  * modified versions are required to be marked in reasonable ways as
  * different from the original version (for example by appending a copyright notice).
- * 
+ *
  * Linking this library statically or dynamically with other modules is making a
  * combined work based on this library. Thus, the terms and conditions of the
  * GNU Affero General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this library give you 
- * permission to link this library with independent modules to produce an 
+ *
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
  * executable, regardless of the license terms of these independent modules,
- * and to copy and distribute the resulting executable under terms of your 
+ * and to copy and distribute the resulting executable under terms of your
  * choice, provided that you also meet, for each linked independent module,
  * the terms and conditions of the license of that module. An independent
  * module is a module which is not derived from or based on this library.
  */
 package aphelion.shared.physics;
 
-/**
- *
- * @author Joris
- */
-public enum WEAPON_SLOT
-{
-        GUN(0, "weapon-slot-gun"),
-        GUN_MULTI(1, "weapon-slot-gun-multi"),
-        BOMB(2, "weapon-slot-bomb"),
-        MINE(3, "weapon-slot-mine"),
-        BURST(4, "weapon-slot-burst"),
-        REPEL(5, "weapon-slot-repel"),
-        DECOY(6, "weapon-slot-decoy"),
-        THOR(7, "weapon-slot-thor"),
-        BRICK(8, "weapon-slot-brick"),
-        ROCKET(9, "weapon-slot-rocket"),
-        ;
-        
-        public final int id; // can be used as an array key
-        public final String settingName;
-        private final static WEAPON_SLOT[] values = values();
+import org.junit.Test;
 
-        private WEAPON_SLOT(int id, String settingName)
+import static org.junit.Assert.*;
+
+public class WEAPON_SLOTTest
+{
+        @Test
+        public void testById() throws Exception
         {
-                this.settingName = settingName;
-                this.id = id;
-        }
-        
-        public static WEAPON_SLOT byId(int id)
-        {
+                assertEquals(WEAPON_SLOT.GUN, WEAPON_SLOT.byId(WEAPON_SLOT.GUN.id));
+                assertEquals(WEAPON_SLOT.DECOY, WEAPON_SLOT.byId(WEAPON_SLOT.DECOY.id));
+                assertEquals(WEAPON_SLOT.ROCKET, WEAPON_SLOT.byId(WEAPON_SLOT.ROCKET.id));
+
                 try
                 {
-                        return values[id];
+                        WEAPON_SLOT.byId(-1);
+                        assertTrue(false);
                 }
-                catch (IndexOutOfBoundsException ex)
+                catch(IllegalArgumentException ex)
                 {
-                        throw new IllegalArgumentException("Invalid WEAPON_SLOT id " + id, ex);
+                        assertTrue(true);
+                }
+
+                try
+                {
+                        WEAPON_SLOT.byId(100000);
+                        assertTrue(false);
+                }
+                catch(IllegalArgumentException ex)
+                {
+                        assertTrue(true);
                 }
         }
-        
-        public static boolean isValidId(int id)
+
+        @Test
+        public void testIsValidId() throws Exception
         {
-                return id >= 0 && id < values.length;
+                assertFalse(WEAPON_SLOT.isValidId(-1));
+                assertTrue(WEAPON_SLOT.isValidId(WEAPON_SLOT.GUN.id));
+                assertTrue(WEAPON_SLOT.isValidId(WEAPON_SLOT.BOMB.id));
+                assertTrue(WEAPON_SLOT.isValidId(WEAPON_SLOT.ROCKET.id));
+                assertFalse(WEAPON_SLOT.isValidId(100000));
         }
-}
+
+
+} 
