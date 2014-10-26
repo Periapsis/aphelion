61581452ef0e97c6b531b533283e2a086d5ee144
Joris van der Wel <joris@jorisvanderwel.com>
2014-10-26 16:46:00 +0100

    This test case demonstrates the need for a different forceHistory implementation

--- src/test/java/aphelion/shared/physics/ForceEmitterTest.java
+++ src/test/java/aphelion/shared/physics/ForceEmitterTest.java
@@ -39,6 +39,7 @@ package aphelion.shared.physics;
 
 import aphelion.shared.gameconfig.GameConfig;
 import aphelion.shared.physics.entities.ActorPublic;
+import aphelion.shared.physics.entities.ProjectilePublic;
 import aphelion.shared.physics.valueobjects.PhysicsPoint;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
@@ -63,7 +64,9 @@ public class ForceEmitterTest extends PhysicsTest
                 yamlDocuments = GameConfig.loadYaml(
                         "- weapon-slot-repel: repelA\n" +
                         "  weapon-slot-burst: repelB\n" +
+                        "  weapon-slot-bomb: bomb\n" +
                         "  ship-speed: "+EnvironmentConf.MAX_POSITION+"\n" +
+
                         "- selector: {weapon: [repelA, repelB]}\n" +
                         "  projectile-force-distance-ship: 204800\n" +
                         "  projectile-force-velocity-ship: 3840000\n" +
@@ -79,7 +82,15 @@ public class ForceEmitterTest extends PhysicsTest
                         
                         "- selector: {weapon: [repelB]}\n" +
                         "  projectile-force-velocity-ship: 10\n" +
-                        "  projectile-expiration-ticks: 4\n"
+                        "  projectile-force-velocity-projectile: 20\n" +
+                        "  projectile-expiration-ticks: 4\n" +
+
+                        "- selector: {weapon: [bomb]}\n" +
+                        "  projectile-speed: 3\n" +
+                        "  projectile-expiration-ticks: 1000\n" +
+                        "  projectile-angle-relative: true\n" +
+                        "  projectile-offset-x: 0\n" +
+                        "  projectile-offset-y: 0\n"
                 );
                 
                 }
@@ -113,7 +124,7 @@ public class ForceEmitterTest extends PhysicsTest
                 SECOND_POSITION_START.sub(SECOND_VEL_START);
                 
                 env.actorWarp(0, ACTOR_FIRST , false, FIRST_POSITION_START.x, FIRST_POSITION_START.y, 0, 0, 0);
-                env.actorWarp(0, ACTOR_SECOND, false, SECOND_POSITION_START.x, SECOND_POSITION_START.y, SECOND_VEL_START.x, SECOND_VEL_START.y,     0);
+                env.actorWarp(0, ACTOR_SECOND, false, SECOND_POSITION_START.x, SECOND_POSITION_START.y, SECOND_VEL_START.x, SECOND_VEL_START.y, 0);
                 
                 env.actorWeapon(1, ACTOR_FIRST, WEAPON_SLOT.REPEL);
                 
@@ -169,7 +180,7 @@ public class ForceEmitterTest extends PhysicsTest
                 SECOND_POSITION_START.sub(SECOND_VEL_START);
                 
                 env.actorWarp(0, ACTOR_FIRST , false, FIRST_POSITION_START.x, FIRST_POSITION_START.y, 0, 0, 0);
-                env.actorWarp(0, ACTOR_SECOND, false, SECOND_POSITION_START.x, SECOND_POSITION_START.y, SECOND_VEL_START.x, SECOND_VEL_START.y,     0);
+                env.actorWarp(0, ACTOR_SECOND, false, SECOND_POSITION_START.x, SECOND_POSITION_START.y, SECOND_VEL_START.x, SECOND_VEL_START.y, 0);
                 
                 env.actorWeapon(1, ACTOR_FIRST, WEAPON_SLOT.BURST);
                 
@@ -219,5 +230,112 @@ public class ForceEmitterTest extends PhysicsTest
                 assertVelocity(SECOND_VEL_START.x + 5 + 4 + 4 + 4,
                                SECOND_VEL_START.y, 
                                secondActor);
+        }
+
+        @Test
+        public void testLateAsPossibleWithoutTimeWarp_forceOnProjectile_control()
+        {
+                SimpleEnvironment env = (SimpleEnvironment) this.env;
+
+                env.actorNew(0, ACTOR_FIRST , 1234, "NX-01");
+                env.actorNew(0, ACTOR_SECOND, 1234, "O'Neill");
+
+                final PhysicsPoint FIRST_POSITION_START  = new PhysicsPoint(20000             , 50000);
+                final PhysicsPoint SECOND_POSITION_START = new PhysicsPoint(20000 + 204800 / 2, 50000);
+
+                env.actorWarp(0, ACTOR_FIRST , false, FIRST_POSITION_START.x , FIRST_POSITION_START.y , 0, 0, EnvironmentConf.ROTATION_1_4TH);
+                env.actorWarp(0, ACTOR_SECOND, false, SECOND_POSITION_START.x, SECOND_POSITION_START.y, 0, 0, EnvironmentConf.ROTATION_1_4TH * 3);
+
+                env.actorWeapon(1, ACTOR_FIRST, WEAPON_SLOT.BOMB);
+
+                // This is the operation that will arrive late in the next test:
+                env.actorWeapon(7, ACTOR_SECOND, WEAPON_SLOT.BURST); // repelB
+
+                ActorPublic firstActor = env.getActor(ACTOR_FIRST);
+                ActorPublic secondActor = env.getActor(ACTOR_SECOND);
+
+                env.tick(); // 1
+                env.tick(); // 2
+
+                ProjectilePublic projectile = env.projectileIterator().next();
+                assertEquals(ACTOR_FIRST, projectile.getOwner());
+                assertVelocity(3, 0, projectile);
+
+
+                while (env.getTick() < 7) { env.tick(); }
+                assertVelocity(3, 0, projectile);
+
+                // force is applied during 4 ticks
+                env.tick();
+                assertVelocity(3 -15, 0, projectile);
+                env.tick();
+                assertVelocity(3 -15-15, 0, projectile);
+                env.tick();
+                assertVelocity(3 -15-15-14, 0, projectile);
+                env.tick();
+                assertVelocity(3 -15-15-14-14, 0, projectile);
+                env.tick();
+                assertVelocity(3 -15-15-14-14, 0, projectile);
+
+                while (env.getTick() < 40) { env.tick(); }
+
+                assertVelocity(3 -15-15-14-14, 0, projectile);
+                assertPosition(
+                        18288,
+                        FIRST_POSITION_START.y,
+                        projectile);
+        }
+
+        @Test
+        public void testLateAsPossibleWithoutTimeWarp_forceOnProjectile()
+        {
+                SimpleEnvironment env = (SimpleEnvironment) this.env;
+
+                env.actorNew(0, ACTOR_FIRST , 1234, "NX-01");
+                env.actorNew(0, ACTOR_SECOND, 1234, "O'Neill");
+
+                final PhysicsPoint FIRST_POSITION_START  = new PhysicsPoint(20000             , 50000);
+                final PhysicsPoint SECOND_POSITION_START = new PhysicsPoint(20000 + 204800 / 2, 50000);
+
+                env.actorWarp(0, ACTOR_FIRST , false, FIRST_POSITION_START.x , FIRST_POSITION_START.y , 0, 0, EnvironmentConf.ROTATION_1_4TH);
+                env.actorWarp(0, ACTOR_SECOND, false, SECOND_POSITION_START.x, SECOND_POSITION_START.y, 0, 0, EnvironmentConf.ROTATION_1_4TH * 3);
+
+                env.actorWeapon(1, ACTOR_FIRST, WEAPON_SLOT.BOMB);
+
+                ActorPublic firstActor = env.getActor(ACTOR_FIRST);
+                ActorPublic secondActor = env.getActor(ACTOR_SECOND);
+
+                final int PROJECTILE_HISTORY_LENGTH = env.getConfig().TRAILING_STATE_DELAY + env.getConfig().MINIMUM_HISTORY_TICKS;
+                assert PROJECTILE_HISTORY_LENGTH == 34 : "test case needs adjusting";
+
+                env.tick(); // 1
+                env.tick(); // 2
+
+                ProjectilePublic projectile = env.projectileIterator().next();
+                assertEquals(ACTOR_FIRST, projectile.getOwner());
+                assertVelocity(3, 0, projectile);
+
+                while (env.getTick() < 40) { env.tick(); }
+
+                /* */
+                assertVelocity(3, 0, projectile);
+                assertPosition(
+                        FIRST_POSITION_START.x + 3 * ((int) env.getTick() - 1),
+                        FIRST_POSITION_START.y,
+                        projectile);
+
+                // the only difference between this test case and the previous one is that this this operation is late:
+                assert env.getTick() - PROJECTILE_HISTORY_LENGTH + 1 == 7;
+                // this is the oldest tick we can fix without a timewarp
+                env.actorWeapon(env.getTick() - PROJECTILE_HISTORY_LENGTH + 1, ACTOR_SECOND, WEAPON_SLOT.BURST); // repelB
+                /* */
+                env.doReexecuteDirtyPositionPath();
+
+                // see previous test case
+                assertVelocity(3 -15-15-14-14, 0, projectile);
+                assertPosition(
+                        18288,
+                        FIRST_POSITION_START.y,
+                        projectile);
         }
 }
