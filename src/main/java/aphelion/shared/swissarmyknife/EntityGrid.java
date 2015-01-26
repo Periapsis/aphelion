aedc66c686154364d22eb0b6e9801ae6467d6f96
Joris van der Wel <joris@jorisvanderwel.com>
2015-01-26 01:39:51 +0100

    These lines are not supposed to be in the license (see LICENSE.txt)

--- src/main/java/aphelion/shared/swissarmyknife/EntityGrid.java
+++ src/main/java/aphelion/shared/swissarmyknife/EntityGrid.java
@@ -34,9 +34,6 @@
  * choice, provided that you also meet, for each linked independent module,
  * the terms and conditions of the license of that module. An independent
  * module is a module which is not derived from or based on this library.
- * If you modify this library, you may extend this exception to your version
- * of the library, but you are not obliged to do so. If you do not wish to do
- * so, delete this exception statement from your version.
  */
 
 package aphelion.shared.swissarmyknife;
